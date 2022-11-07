10619d7067bde1b00959203d3bb38ea6022c581d
halil ibrahim <halil199010@hotmail.com>
2022-11-07 10:56:06 +0300

    Assignment wekk 2 done

--- Python_Assignments_Week_2.py
+++ Python_Assignments_Week_2.py
@@ -1,3 +1,5 @@
+# halil ibrahim çalış   22435848590
+
 def sayi_harf_say(stringifade):         # 1- Sayı ve Harfleri Say
     sayi_list=["0","1","2","3","4","5","6","7","8","9"]
     stringlist=list(stringifade)       # strng ifadeyi listeye çevirdim ki içindekileri teker teker kontrol edebileyim
@@ -13,8 +15,13 @@ def sayi_harf_say(stringifade):         # 1- Sayı ve Harfleri Say
     print("harfler= ",letters)
     print("sayılar= ", numbers)
 
-def ayin_gunleri():
-    pass
+def ayin_gunleri(ay,yil):
+    daylist=[31,28,31,30,31,30,31,31,30,31,30,31]
+    if yil%4==0:            # yıl artık yıl ise
+        daylist[1]=29       # şubatı 29 çektir
+    print("{}. yılın {}. ayı {} çeker".format(yil,ay,daylist[ay-1]))
+
+
 def kucuk_buyuk_sayılar(stringifade):       # 3. Küçük ve Büyük Sayıları
 
     smallest=10
@@ -31,7 +38,7 @@ def kucuk_buyuk_sayılar(stringifade):       # 3. Küçük ve Büyük Sayıları
             temp=i+1                                        # sonraki döngüyü atla
         else:
             intlist.append(int(stringifade[i]))             # negatif sayı değilse direk al
-    for i in range(len(intlist)):                   # en büyük ve küçük sayıyı bulma döngüsü
+    for i in range(len(intlist)):                   # en büyük ve küçük sayıyı bulma döngüsü. max() min() diye fonksiyonlar olduğunu bilmiodum
         if intlist[i]<smallest:
             smallest=intlist[i]
         if intlist[i]>biggest:
@@ -40,5 +47,24 @@ def kucuk_buyuk_sayılar(stringifade):       # 3. Küçük ve Büyük Sayıları
     print(biggest," ", smallest)
 
 
-def Bes_harf_cevir():
+def Bes_harf_cevir(word):
+    wordlist=word.split(" ")            # kelimeleri listeye atadım
+    for i in range(len(wordlist)):
+        if len(wordlist[i])>=5:         # kelime beşten büyükse
+            revstr=wordlist[i]          # kelimeyi değişkene ata
+            wordlist[i]=revstr[::-1]    # değişkenin tersini al ve liste elemanına ata
+    print(wordlist)
     pass
+
+def Orta_harf(stringifade):
+    l=len(stringifade)
+    if l%2==0:
+        print(stringifade[l//2-1],stringifade[l//2],sep="")
+    elif l==1:
+        print(stringifade)
+    else:
+        print(stringifade[l//2])
+
+
+
+Bes_harf_cevir("naber kanka nasilsin")
